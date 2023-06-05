3286d96d055f498abe6ad70f17cdb9b7b3a1dbc5
voolyvex <1joeknapp@gmail.com>
2023-06-04 19:51:38 -0500

    Completed string_repeat kata

--- CodingChallengesRuby/kata/string_repeat.rb
+++ CodingChallengesRuby/kata/string_repeat.rb
@@ -1,3 +1,16 @@
 # Write a function that accepts an integer n and a string s 
 # as parameters, and returns a string of s repeated exactly n times.
 
+def string_repeater(n, s)
+  count = 0
+  repeated_string = ""
+
+  until count >= n do
+      repeated_string << s
+      count += 1
+  end
+
+  return repeated_string
+end
+
+puts string_repeater(999, "Bananas!!bananas!!!Banaanaaas!")
