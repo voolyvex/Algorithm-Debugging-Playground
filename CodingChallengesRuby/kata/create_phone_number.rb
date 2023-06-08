9a0908c46f1b191a3b8489b0f27303487d028434
voolyvex <1joeknapp@gmail.com>
2023-06-08 16:24:59 -0500

    Completed kata

---
+++ CodingChallengesRuby/kata/create_phone_number.rb
@@ -0,0 +1,14 @@
+#Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+#Example
+#createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+#The returned format must be correct in order to complete this challenge.
+
+# Don't forget the space after the closing parentheses!
+
+def create_phone_number(numbers)
+  return "(#{numbers[0..2].join}) #{numbers[3..5].join}-#{numbers[6..9].join}"
+end
+
+testcase1 = [5,4,3,2,1,0,9,8,7,6]
+puts create_phone_number(testcase1)
